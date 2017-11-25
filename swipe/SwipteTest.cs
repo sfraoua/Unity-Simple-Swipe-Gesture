10c3385b6e8d603ccfbd57da567529c148b64169
slim fraoua <sfraoua@gmail.com>
2017-11-25 11:24:02 +0100

    update

--- swipe/SwipteTest.cs
+++ swipe/SwipteTest.cs
@@ -2,30 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwipteTest : MonoBehaviour {
+public class SwipteTest : MonoBehaviour
+{
     public Swipe swipeControls;
     public Transform player;
     private Vector3 desiredPosition;
 
-	// Use this for initialization
-	void Start () {
-        Debug.Log("START");
-
-        Debug.Log(desiredPosition);
-        Debug.Log(player.transform.position);
-        Debug.Log("-------------------");
+    // Use this for initialization
+    void Start()
+    {
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         if (swipeControls.SwipeLeft)
-            desiredPosition += (Vector3.left + Vector3.back);
+            desiredPosition += Vector3.left;
         else if (swipeControls.SwipeRight)
-            desiredPosition += (Vector3.right + Vector3.forward);
+            desiredPosition += Vector3.right;
         else if (swipeControls.SwipeUp)
-            desiredPosition += (Vector3.forward + Vector3.left);
+            desiredPosition += Vector3.forward;
         else if (swipeControls.SwipeDown)
-            desiredPosition += (Vector3.back + Vector3.right);
+            desiredPosition += Vector3.back;
 
 
         player.transform.position = Vector3.MoveTowards(player.transform.position, desiredPosition, 3f * Time.deltaTime);
